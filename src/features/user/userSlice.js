9d0ac7d927737499fb4db100b338cd3ab77b270c
calevv <ika818488@gmail.com>
2025-07-31 15:40:57 +0900

    first commit

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -15,12 +15,33 @@ export const loginWithGoogle = createAsyncThunk(
 );
 
 export const logout = () => (dispatch) => {};
+
 export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (
     { email, name, password, navigate },
     { dispatch, rejectWithValue }
-  ) => {}
+  ) => {
+    try {
+      const res = await api.post("/user", { email, name, password });
+      dispatch(
+        showToastMessage({
+          message: "회원가입을 성공했습니다.",
+          status: "success",
+        })
+      );
+      navigate("/login");
+      return res.data.data;
+    } catch (err) {
+      dispatch(
+        showToastMessage({
+          message: "회원가입을 실패했습니다.",
+          status: "error",
+        })
+      );
+      rejectWithValue(err.error);
+    }
+  }
 );
 
 export const loginWithToken = createAsyncThunk(
@@ -43,7 +64,19 @@ const userSlice = createSlice({
       state.registrationError = null;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.loading = false;
+        state.registrationError = null;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.registrationError = action.payload;
+      });
+  },
 });
 export const { clearErrors } = userSlice.actions;
 export default userSlice.reducer;
